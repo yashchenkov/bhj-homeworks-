54cc22dee847ab8dafc56eb81a004085a5a6c433
Yashchenko Victor <Andrey@Komp11.local>
2022-02-03 19:44:06 +0300

    исправил кротов

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,26 +2,26 @@ const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
 const holes = document.getElementsByClassName('hole');
-for(item of holes){
-	item.addEventListener('click', () => {
-		if(item.classList.contains('hole_has-mole')){
+for(let i = 0; i < holes.length; i++){
+	holes[i].addEventListener('click', () => {
+		if(holes[i].classList.contains('hole_has-mole')){
 			dead.textContent = Number(dead.textContent) + 1;
 		} else {
 			lost.textContent = Number(lost.textContent) + 1;
 		}
 
 		if(Number(dead.textContent) === 10) {
-			window.alert('Вы выиграли');
-			dead.textContent = 0;
-			lost.textContent = 0;
+			windowAlert('Вы выиграли!');
 		} else if(Number(lost.textContent) === 3) {
-			window.alert('Вы проиграли!');
-			dead.textContent = 0;
-			lost.textContent = 0;
+			windowAlert('Вы проиграли!');
 		}
 	});
 }
 
-
+function windowAlert(text) {
+	window.alert(text);
+	dead.textContent = 0;
+	lost.textContent = 0;
+}
 
 
